d6e119488f5e6457a112f468bd6f79aca3725977
pabloNaveira <pablonaveiralopez@gmail.com>
2021-03-18 22:30:07 +0100

    --styling

---
+++ js/elect.js
@@ -0,0 +1,9 @@
+var prueba = document.getElementById("prueba");
+
+
+function añadir() {
+    prueba.innerHTML = '<a href="../index.html"><img src="../img/skills.jpg"></a>';
+    console.log(prueba);
+}
+
+añadir();
--- js/main.js
+++ js/main.js
@@ -21,14 +21,14 @@ var holdActive = false;
 
 btn.mousedown(onMouseDown);
 function onMouseDown(){
-
+    btn.css("border", "1px solid var(--rosa");
 
 
     holdStarter = setTimeout(function() {
 		holdStarter = null;
 		holdActive = true; 
         
-
+        console.log(btn.css("border"))
 
         let count = 0;
 
@@ -39,13 +39,12 @@ function onMouseDown(){
             console.log(count);
             let percent = count + "%";
 
-            console.log(percent);
-  
+            
           btn.css("background-size", "100%" + percent);
 
           if (count == 100) {
 
-            window.open('https://www.codexworld.com', '_blank');
+            window.open('../pages/elect.html', '_blank');
 
 
 
@@ -78,9 +77,8 @@ function onMouseUp(){
         btn.css("border", "1px solid var(--rosa)");
 
 		clearTimeout(holdStarter);
-
-        console.log("click")
 	}
+
 	else if (holdActive) {
 		holdActive = false;
        console.log("paró de presionar")
@@ -94,7 +92,116 @@ btn.mouseout( function(){
 
 
 
-
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area": 789.1476416322727
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.48927153781200905,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 0.2,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 3,
+          "size_min": 0,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.2,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 83.91608391608392,
+          "size": 1,
+          "duration": 3,
+          "opacity": 1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
 
 
 
